9dcb811f825e0b2fb003aeb4c76d5c2d9343d0cc
godzhang <zhangqijilindaxue@126.com>
2021-06-16 07:26:53 +0800

    vue3-study

---
+++ vue3-study-bilibili/src/components/LifeCycle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="life-cycle">生命周期</div>
+</template>
+<script lang="ts">
+import {defineComponent, onMounted} from 'vue'
+
+export default defineComponent({
+  setup(props, context) {
+    console.log('setup')
+
+    onMounted(() => {
+      console.log('setup onMounted')
+    })
+
+    return {
+      
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted(){
+    console.log('mounted')
+  }
+})
+</script>
+<style scoped>
+</style>
